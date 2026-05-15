663101c419ce1e494541a4a6fdbe950cd59a31b4
qx0Abi <badstring.legit@gmail.com>
2026-05-15 06:56:02 +0200

    Clear existing partial file

--- WonkaHub.lua
+++
@@ -1,8 +0,0 @@
-if getgenv().WonkaHub_Running then
-    warn("Script already running!")
-    return
-end
-
-getgenv().WonkaHub_Running = true
-
-print("WonkaHub loaded - full script too large for single upload via tool. Consider splitting or using Git locally.")
